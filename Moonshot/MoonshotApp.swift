6eb886ca460835a108ecad89084bc65bcc395f15
Kris Laratta <krislaratta@gmail.com>
2022-10-13 15:49:05 -0400

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kris Laratta on 10/13/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
