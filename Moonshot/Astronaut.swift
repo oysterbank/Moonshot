84910a3b99be5449488fe7f1cdecb18129977bbd
Kris Laratta <krislaratta@gmail.com>
2022-10-13 20:09:48 -0400

    see missions and astronauts

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Kris Laratta on 10/13/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
